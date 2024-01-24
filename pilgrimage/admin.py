84ad12fcbb093a2130930f45e22770b3b454a2dc
MaheshNagaladinne <maheshnagaladinne@gmail.com>
2024-01-25 01:43:04 +0530

    added a superadmin

--- pilgrimage/admin.py
+++ pilgrimage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, Booking, Pilgrim
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Booking)
+admin.site.register(Pilgrim)
